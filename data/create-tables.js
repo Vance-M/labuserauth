7b7c3cc17a8b298ee52eefcae3ce316cafbab4f3
Vance-M <myveryvills@yahoo.com>
2021-03-01 21:51:43 -0800

    nothing

--- data/create-tables.js
+++ data/create-tables.js
@@ -9,7 +9,6 @@ async function run() {
   try {
     // initiate connecting to db
     await client.connect();
-
     // run a query to create tables
     await client.query(`
                 CREATE TABLE users (
@@ -17,17 +16,17 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE todos (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    todo VARCHAR(512) NOT NULL,
+                    completed BOOLEAN NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
 
     console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
   }
-  catch(err) {
+  catch (err) {
     // problem? let's see the error...
     console.log(err);
   }
---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  {
+    todo: 'do the laundry',
+    completed: false,
+  },
+  {
+    todo: 'take out the trash',
+    completed: false,
+  },
+];
